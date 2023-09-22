8c234fa2d80aeb11a1d4cb7a1cfcbfd18e3e62b2
Alina Bregvadze <alina.bregvadze@gmail.com>
2023-09-22 13:39:20 +0400

    add main page

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+if (document.querySelector(".main-page-swiper")) {
+	const swiper = new Swiper(".main-page-swiper", {
+		navigation: {
+			nextEl: ".main-page-swiper .swiper-button-next",
+			prevEl: ".main-page-swiper .swiper-button-prev",
+		},
+	});
+}
+
+if (document.querySelector(".main-page-news")) {
+	const swiper = new Swiper(".swiper-main-page-news-swiper", {
+		navigation: {
+			nextEl: ".main-page-news .swiper-button-next",
+			prevEl: ".main-page-news .swiper-button-prev",
+		},
+		slidesPerView: 4,
+		spaceBetween: 16,
+		breakpoints: {
+			0: {
+				slidesPerView: 1,
+				spaceBetween: 20,
+			},
+			1024: {
+				slidesPerView: 4,
+				spaceBetween: 16,
+			},
+		},
+	});
+}
